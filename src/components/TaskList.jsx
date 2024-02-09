cf0d4cb426cfdc7e8f26a8cdc2e8e6e69cdcbb1e
Shahriar Turag <shahriarturag.st@gmail.com>
2024-02-09 20:50:40 +0600

    set up the project

---
+++ src/components/TaskList.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Task } from './Task';
+
+export const TaskList = () => {
+	return (
+		<div>
+			<h2>Tasks</h2>
+			<ul>
+				<Task />
+			</ul>
+		</div>
+	);
+};
